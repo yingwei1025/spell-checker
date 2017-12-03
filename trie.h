8215e9da35587cf262074d4460af650f275c57c5
Yw <yingwei1025@gmail.com>
2017-12-03 09:42:07 +0800

    trie algorithm

---
+++ trie.h
@@ -0,0 +1,82 @@
+#define ARRAY_SIZE(a) sizeof(a)/sizeof(a[0])
+ 
+// Alphabet size (# of symbols)
+#define ALPHABET_SIZE (94)
+ 
+// Converts key current character into index
+// use only 'a' through 'z' and lower case
+#define CHAR_TO_INDEX(c) ((int)c - (int)'!')
+ 
+// trie node
+struct TrieNode
+{
+    struct TrieNode *children[ALPHABET_SIZE];
+ 
+    // isEndOfWord is true if the node represents
+    // end of a word
+    bool isEndOfWord;
+};
+ 
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void)
+{
+    struct TrieNode *pNode = NULL;
+ 
+    pNode = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+ 
+    if (pNode)
+    {
+        int i;
+ 
+        pNode->isEndOfWord = false;
+ 
+        for (i = 0; i < ALPHABET_SIZE; i++)
+            pNode->children[i] = NULL;
+    }
+ 
+    return pNode;
+}
+ 
+// If not present, inserts key into trie
+// If the key is prefix of trie node, just marks leaf node
+void insert(struct TrieNode *root, const char *key)
+{
+    int level;
+    int length = strlen(key);
+    int index;
+ 
+    struct TrieNode *pCrawl = root;
+ 
+    for (level = 0; level < length; level++)
+    {
+        index = CHAR_TO_INDEX(key[level]);
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = getNode();
+ 
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    // mark last node as leaf
+    pCrawl->isEndOfWord = true;
+}
+ 
+// Returns true if key presents in trie, else false
+bool search(struct TrieNode *root, const char *key)
+{
+    int level;
+    int length = strlen(key);
+    int index;
+    struct TrieNode *pCrawl = root;
+ 
+    for (level = 0; level < length; level++)
+    {
+        index = CHAR_TO_INDEX(key[level]);
+ 
+        if (!pCrawl->children[index])
+            return false;
+ 
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    return (pCrawl != NULL && pCrawl->isEndOfWord);
+}
